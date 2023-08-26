fa1c45e39c6206c93ba0eaa43c38ce6b13fb678e
Suphakit P <me@suphakit.net>
2023-08-26 13:03:59 +0700

    feat<module>: added quote based

---
+++ src/modules/quote.js
@@ -0,0 +1,38 @@
+const quotes = [
+  {
+    quote: `You really enjoy hurting people, don't you`,
+    by: 'Richard 🐔'
+  },
+  {
+    quote: `Leaving this world is not as scary as it sounds`,
+    by: 'Richard 🐔'
+  },
+  {
+    quote: `Do you like hurting other people?`,
+    by: 'Richard 🐔'
+  },
+  {
+    quote: `You're dead meat...`,
+    by: 'Biker 🏍'
+  },
+  {
+    quote: `Why don't you finish what you've started?`,
+    by: 'GirlFriend 🙍🏼‍♀️'
+  },
+  {
+    quote: `You had your chance.`,
+    by: 'Biker 🏍'
+  },
+  {
+    quote: `Someone you know is not who you think he is.`,
+    by: 'Richard 🐔'
+  },
+  {
+    quote: `Something will soon be taken from you.`,
+    by: 'Richard 🐔'
+  },
+  {
+    quote: `...`,
+    by: 'Jacket 🧥'
+  }
+]
