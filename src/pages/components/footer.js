a2c8c9658a14911bdc414a39d80d2d0b9e07e5c7
Suphakit P <me@suphakit.net>
2023-08-26 15:25:41 +0700

    feat<components>: added footer

---
+++ src/pages/components/footer.js
@@ -0,0 +1,7 @@
+const Footer = `<div class="footer">
+  <p>
+    All of Quotes by <a href="https://dennatongames.com/" target="_blank">Dennaton Games</a>, under <a href="https://www.devolverdigital.com/" target="_blank">Devolver Digital</a>&nbsp;&nbsp;|&nbsp;&nbsp;Made w/ 🤍 by <a href="https://suphakit.net">Suphakit P.</a>
+  </p>
+</div>`
+
+module.exports = Footer
