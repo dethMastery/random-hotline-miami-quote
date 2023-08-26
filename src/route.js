ca3fa5114a3f6aa47a95ff32fe8546a0cdc7159c
Suphakit P <me@suphakit.net>
2023-08-26 15:47:27 +0700

    feat<route>: added new routes

--- src/route.js
+++ src/route.js
@@ -1,6 +1,22 @@
+const quote = require('./modules/quote')
+const Index = require('./pages/index')
+const About = require('./pages/about')
+
 function Route(app, root) {
   app.get('/', (req, res) => {
-    res.send('server spawned')
+    const Random = Math.floor(Math.random() * quote.length)
+
+    res.send(Index(quote[Random]))
+  })
+
+  app.get('/about', (req, res) => {
+    res.send(About)
+  })
+
+  app.get('/api/quotes', (req, res) => {
+    const Random = Math.floor(Math.random() * quote.length)
+
+    res.json(quote[Random])
   })
 }
 
