67f051d908524991f55259ed1d36c59185b8818c
Suphakit P <me@suphakit.net>
2023-08-26 15:19:03 +0700

    feat<components>: added navbar components

---
+++ src/pages/components/navbar.js
@@ -0,0 +1,14 @@
+const NavBar = `<div class="nav">
+  <div class="left">
+    <a href="/">
+      Hotline Quotes
+    </a>
+  </div>
+  <div class="right">
+    <a href="/about">
+      About
+    </a>
+  </div>
+</div>`
+
+module.exports = NavBar
